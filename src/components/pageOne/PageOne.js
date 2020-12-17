5811f30836b2589913527efefb6034520ea21b1e
Alfred <castellum.a@gmail.com>
2020-12-17 19:24:35 +0100

    Created the beginning of page One.

--- src/components/pageOne/PageOne.js
+++ src/components/pageOne/PageOne.js
@@ -1,24 +1,47 @@
-import Navbar from "./../navbar/Navbar";
-import Footer from "./../footer/Footer";
-import { Context } from "./../../context/Context";
-import { ApiContext } from "../../context/ApiContext";
+// import Navbar from "./../navbar/Navbar";
+// import Footer from "./../footer/Footer";
+import React, { Component } from 'react';
+import './PageOne.css'
 
 /* WHEN YOU NEED DATA OF API CALL FOR api.something WHEN YOU NEED DATA FROM CONTEXT CALL value.something */
 
-const PageOne = () => (
-  <Context.Consumer>
-    {(value) => (
-      <ApiContext.Consumer>
-        {(api) => (
-          <>
-            <Navbar />
-            <>MY CONTENT</>
-            <Footer />
-          </>
-        )}
-      </ApiContext.Consumer>
-    )}
-  </Context.Consumer>
-);
+const ediblePlants = 'https://trefle.io/api/v1/plants?&token=lFM81UTKUliWbUM-9QDM0m3X8jPYbyFlAQrIQAFcRZA&filter[edible]=true';
+
+const apiCaller = url => fetch(url).then(result => result.json())
+
+class PageOne extends Component {
+
+  state = {
+    ediblePlant: []
+  }
+
+  componentDidMount() {
+    apiCaller(ediblePlants)
+      .then(data =>
+        this.setState({
+          ediblePlant: data.data
+        })
+      )
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Edible Plants</h1>
+        <div className='flexContainer'>
+          {
+            this.state.ediblePlant.map((iterator, index) =>
+              <div className='plantContainer' key={index}>
+                <h3>{iterator.common_name}</h3>
+                  <img id='pageOneImage' src={iterator.image_url} alt={iterator.scientific_name}></img>
+              </div>
+            )
+          }
+        </div>
+      </div>
+    )
+  }
+
+}
 
 export default PageOne;
